7432ee452e30ff86661b197a835cbb33b0f203bb
LuisGeraldo <lrafaelcg@gmail.com>
2018-11-05 03:59:50 -0400

    En esta version se agrego las validacion, el manejo de usuario y se creo la vista para el empleado

--- target/generated-sources/annotations/com/gestion/compras/entities/TipoUsuario_.java
+++ target/generated-sources/annotations/com/gestion/compras/entities/TipoUsuario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-01T19:35:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-05T03:57:49")
 @StaticMetamodel(TipoUsuario.class)
 public class TipoUsuario_ { 
 
