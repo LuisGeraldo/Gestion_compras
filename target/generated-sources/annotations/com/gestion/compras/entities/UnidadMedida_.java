5ee5ec97d7d6b6e0507e966d30a42b7dc8d01133
LuisGeraldo <lrafaelcg@gmail.com>
2018-11-08 14:47:51 -0400

    Prueba

--- target/generated-sources/annotations/com/gestion/compras/entities/UnidadMedida_.java
+++ target/generated-sources/annotations/com/gestion/compras/entities/UnidadMedida_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-05T03:57:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-08T14:33:59")
 @StaticMetamodel(UnidadMedida.class)
 public class UnidadMedida_ { 
 
